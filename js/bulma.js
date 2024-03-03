cf63de1d092a4e109fe1716a16fa870e3e096737
damianfarber <farberdamian@gmail.com>
2024-03-03 01:53:27 -0300

    Fixed navbar

--- js/bulma.js
+++ js/bulma.js
@@ -1,10 +1,35 @@
-// The following code is based off a toggle menu by @Bradcomp
-// source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
-(function() {
-    var burger = document.querySelector('.burger');
-    var menu = document.querySelector('#'+burger.dataset.target);
-    burger.addEventListener('click', function() {
-        burger.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Add a click event on each of them
+    $navbarBurgers.forEach(el => {
+      el.addEventListener('click', () => {
+  
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+  
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+  
+      });
     });
-})();
+  
+    // Close the menu when a navigation item is clicked
+    const $navbarItems = Array.prototype.slice.call(document.querySelectorAll('.navbar-item'), 0);
+    $navbarItems.forEach(el => {
+      el.addEventListener('click', () => {
+        // Get the target from the "data-target" attribute
+        const target = el.closest('.navbar').querySelector('.navbar-burger').dataset.target;
+        const $target = document.getElementById(target);
+  
+        // Remove the "is-active" class from both the "navbar-burger" and the "navbar-menu"
+        document.querySelector('.navbar-burger').classList.remove('is-active');
+        $target.classList.remove('is-active');
+      });
+    });
+  
+  });
